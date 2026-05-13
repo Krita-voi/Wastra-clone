301eb5d36b8ff90bf6e5ee47c25413cfc302eb23
krita-voi <kritagyamaharjan794@gmail.com>
2026-05-13 21:57:13 +0545

    feat: Card component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import NavLink from "./components/NavLink";
 import Foot from "./components/Foot";
+import Card from "./components/ClothCard";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <NavBar />
       <NavLink />
       <main className="app-main">
+        <Card />
         <Routes></Routes>
       </main>
       <Foot />
